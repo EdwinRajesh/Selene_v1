d8ad22b2ee01a7ed88545bb7d7641d2ff80f7f72
Sanjid9633 <sanjidsajeeb@gmail.com>
2025-01-10 12:25:48 +0530

    changes to ai codes

--- app/screens/home-screen/GrokApi.js
+++ app/screens/home-screen/GrokApi.js
@@ -1,20 +1,20 @@
 import Groq from "groq-sdk";
 
-// Replace 'your-api-key-here' with your actual API key
+
 const groq = new Groq({ apiKey: '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' });
 
 /**
- * Fetches data from Groq using the Groq client.
- * @param {Array} messages - An array of message objects for the chatbot.
- * @returns {Object} - The response from Groq.
+ 
+ * @param {Array} messages 
+ * @returns {Object} 
  */
 export const fetchDataFromGrok = async (messages) => {
   try {
     const completion = await groq.chat.completions.create({
       messages,
-      model: "llama-3.3-70b-versatile", // Replace with the desired model
+      model: "llama-3.3-70b-versatile", 
     });
-    return completion.choices[0]?.message?.content; // Return the content of the chatbot's response
+    return completion.choices[0]?.message?.content; 
   } catch (error) {
     console.error("Error fetching data from Groq:", error);
     throw error;
